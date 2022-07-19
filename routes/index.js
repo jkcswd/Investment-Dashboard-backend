294ee5bb288972ac8e89cdcc7ec15df430f736f7
James Spencer <jameskcs1254@gmail.com>
2022-07-19 18:25:58 +0100

    change json response and api route

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,17 @@ var router = express.Router();
 var db = require("../database.js")
 
 
-router.get("/", (req, res, next) => {
+router.get("/user", (req, res, next) => {
   var sql = "SELECT * FROM user"
   var params = []
+  
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({"error":err.message});
         return;
       }
-      res.json({
-          "message":"success",
-          "data":rows
-      })
-    });
+      res.json(rows)
+  });
 });
 
 module.exports = router;
