7bea43c65b0e3117f9ab4fc4bc2d8311c97c9b5f
James Spencer <jameskcs1254@gmail.com>
2022-07-19 17:54:04 +0100

    alter fields of table created

--- database.js
+++ database.js
@@ -3,30 +3,25 @@ var sqlite3 = require('sqlite3').verbose()
 const DBSOURCE = "db.sqlite"
 
 const db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
-        console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text, 
-            email text UNIQUE, 
-            password text, 
-            CONSTRAINT email_unique UNIQUE (email)
-            )`,
-        (err) => {
-            if (err) {
-                // Table already created
-            }else{
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
-                db.run(insert, ["admin","admin@example.com",md5("admin123456")])
-                db.run(insert, ["user","user@example.com",md5("user123456")])
-            }
-        });  
-    }
+  if (err) {
+    // Cannot open database
+    console.error(err.message)
+    throw err
+  }else{
+    console.log('Connected to the SQLite database.')
+    db.run(`CREATE TABLE user (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+        )`,
+    (err) => {
+      if (err) {
+        // Table already created
+      }else{
+        // Table just created, creating some rows
+        var insert = 'INSERT INTO user (name) VALUES (?)'
+      }
+    });  
+  }
 });
 
 
