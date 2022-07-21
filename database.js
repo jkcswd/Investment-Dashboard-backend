3d93248bea459d2d515fd2458aee25042b92202d
James Spencer <jameskcs1254@gmail.com>
2022-07-21 08:24:59 +0100

    Clean up db connection function call

--- database.js
+++ database.js
@@ -1,26 +1,11 @@
 var sqlite3 = require('sqlite3').verbose()
 
-const DBSOURCE = "db.sqlite"
-
-const db = new sqlite3.Database(DBSOURCE, (err) => {
+const db = new sqlite3.Database('db.sqlite', (err) => {
   if (err) {
-    // Cannot open database
     console.error(err.message)
     throw err
   }else{
     console.log('Connected to the SQLite database.')
-    db.run(`CREATE TABLE user (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT
-        )`,
-    (err) => {
-      if (err) {
-        // Table already created
-      }else{
-        // Table just created, creating some rows
-        var insert = 'INSERT INTO user (name) VALUES (?)'
-      }
-    });  
   }
 });
 
