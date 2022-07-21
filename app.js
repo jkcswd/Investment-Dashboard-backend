ce119998f1e1686e3563402b897959259d0c6e26
James Spencer <jameskcs1254@gmail.com>
2022-07-21 09:36:31 +0100

    edit comment

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // send error message via JSON
   res.status(err.status || 500);
   res.json(`ERROR: ${err}` );
 });
