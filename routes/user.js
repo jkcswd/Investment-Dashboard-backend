63fcd8002e2c9ef9f8e803f6f283907553270400
James Spencer <jameskcs1254@gmail.com>
2022-07-21 09:12:06 +0100

    change to const with require, delete controller.

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,9 @@
-var express = require('express');
-var router = express.Router();
-var db = require("../database");
+const express = require('express');
+const router = express.Router();
+const {db, query} = require("../database");
 
 router.get("/user", (req, res, next) => {
-  const sql = "SELECT * FROM user"
-  const params = []
   
-  db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(400).json({"error":err.message});
-        return;
-      }
-      res.json(rows)
-  });
 });
 
 module.exports = router;
