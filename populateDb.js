87b20dd38287538d6691ad2d9b7d4f3464075069
James Spencer <jameskcs1254@gmail.com>
2022-07-22 18:21:57 +0100

    Improve naming. Update query with unique

--- populateDb.js
+++ populateDb.js
@@ -4,19 +4,19 @@ const {priceDb, query} = require("./database.js");
 // Yahoo finance tickers
 const getTickerPriceHistory = async (ticker) => {
   const results = await yahooFinance.historical(ticker, { period1: '1900-01-01' });
-  const tickerObj = { ticker, results };
+  const tickerPriceObj = { ticker, results };
   return tickerPriceObj;
 }
 
 const addPriceDataToDb = (tickerPriceObj) => {
-  const name = tickerObj.ticker;
-  const values = tickerObj.results;
+  const name = tickerPriceObj.ticker;
+  const values = tickerPriceObj.results;
 
   priceDb.serialize( async () => {
-    await query(priceDb, `CREATE TABLE IF NOT EXISTS ${name} (date text, open real, high real, low real, close real, adjClose real, volume integer)`, 'run');
+    await query(priceDb, `CREATE TABLE IF NOT EXISTS ${name} (date text UNIQUE, open real, high real, low real, close real, adjClose real, volume integer)`, 'run');
 
     for (const value of values) {
-      await query(priceDb, `INSERT INTO ${name} VALUES("${value.date}", ${value.open}, ${value.high}, ${value.low}, ${value.close}, ${value.adjClose}, ${value.volume})`, 'run')
+      await query(priceDb, `INSERT OR IGNORE INTO ${name} VALUES("${value.date}", ${value.open}, ${value.high}, ${value.low}, ${value.close}, ${value.adjClose}, ${value.volume})`, 'run')
     }
   });
 }
