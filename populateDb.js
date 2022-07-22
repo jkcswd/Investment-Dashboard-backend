cc799901b480bc8bca483ae13c191d9d35264538
James Spencer <jameskcs1254@gmail.com>
2022-07-22 17:16:44 +0100

    Fix addDataToDb to iterate through data for insert

--- populateDb.js
+++ populateDb.js
@@ -4,14 +4,20 @@ const {priceDb, query} = require("./database.js");
 // Yahoo finance tickers
 const getTickerHistory = async (ticker) => {
   const results = await yahooFinance.historical(ticker, { period1: '1900-01-01' });
-
-  return results;
+  const tickerObj = { ticker, results };
+  return tickerObj;
 }
 
-const addDataToDb = (ticker) => {
+const addDataToDb = (tickerObj) => {
+  const name = tickerObj.ticker;
+  const values = tickerObj.results;
+
   priceDb.serialize( async () => {
-    await query(`CREATE TABLE IF NOT EXISTS ${ticker} (date text, open real, high real, low real, close real, adjClose real, volume integer)`, 'run')
-    await query(`INSERT INTO ${ticker} VALUES(.......)`, 'run')
+    await query(`CREATE TABLE IF NOT EXISTS ${name} (date text, open real, high real, low real, close real, adjClose real, volume integer)`, 'run');
+
+    values.forEach(element => {
+      await query(`INSERT INTO ${name} VALUES("${element.date}", ${element.open}, ${element.high}, ${element.low}, ${element.close}, ${element.adjClose}, ${element.volume})`, 'run')
+    }); 
   })
 }
 
