1d36ed088a80624be204fb64dcc1e7db60767e13
James Spencer <jameskcs1254@gmail.com>
2022-07-24 09:26:35 +0100

    Add trailing zeroes to date function

--- populateDb.js
+++ populateDb.js
@@ -28,9 +28,11 @@ const getTickerPriceHistory = async (ticker) => {
 }
 
 const extractDateString = (date) => {
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
   const year = date.getFullYear();
+  if(day < 10 ) { day = `0${day}` };
+  if(month < 10 ) { month = `0${month}` };
 
   return `${year}-${month}-${day}`;
 }
