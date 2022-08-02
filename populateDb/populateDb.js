c98b05a539f1c07826df5fbec45513d06c3b8ab3
James Spencer <jameskcs1254@gmail.com>
2022-08-02 13:19:16 +0100

    add conditional execution on cli

--- populateDb/populateDb.js
+++ populateDb/populateDb.js
@@ -1,9 +1,10 @@
 const populatePriceData = require('./populateStocks.js')
+const checkForPopulate = require('./cli.mjs')
 // TODO: earnings data
 // TODO: economic data
 // TODO: other asset data
-// TODO: CLI interface
 
 (function main (){
-  populatePriceData();
+  const checkForStockPricePopulate = checkForPopulate('stock price');
+  if (checkForStockPricePopulate == 'y') { populatePriceData(); }
 })();
