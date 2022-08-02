1680ebafda7983c15f9dc164e3d0095f47cfb21d
James Spencer <jameskcs1254@gmail.com>
2022-08-02 15:16:07 +0100

    Add conditional calls for population

--- populateDb/populateDb.js
+++ populateDb/populateDb.js
@@ -4,8 +4,15 @@ const checkForPopulate = require('./cli.js');
 const main = async () => {
   const checkForStockPricePopulate = await checkForPopulate('stock price');
   if (checkForStockPricePopulate == 'y') { populatePriceData('stock'); }
+
   const checkForOtherPricePopulate = await checkForPopulate('other asset price');
   if (checkForOtherPricePopulate == 'y') { populatePriceData('other'); }
+
+  const checkForEarningsDataPopulate = await checkForPopulate('stock earnings');
+  if (checkForOtherPricePopulate == 'y') { populateEarnings(); }
+
+  const checkForEconomicDataPopulate = await checkForPopulate('economic');
+  if (checkForOtherPricePopulate == 'y') { populateEconomicData(); }
 }
 
 setTimeout(() => { //allow to connect to DBs before sending prompts
