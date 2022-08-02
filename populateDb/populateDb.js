d3e6f823ce957a3f2c45d7449298310ba52fdc0e
James Spencer <jameskcs1254@gmail.com>
2022-08-02 14:05:23 +0100

    delete comments

--- populateDb/populateDb.js
+++ populateDb/populateDb.js
@@ -1,8 +1,5 @@
-const populatePriceData = require('./populateStocks.js')
-const checkForPopulate = require('./cli.js')
-// TODO: earnings data
-// TODO: economic data
-// TODO: other asset data
+const populatePriceData = require('./populateStocks.js');
+const checkForPopulate = require('./cli.js');
 
 const main = async () => {
   const checkForStockPricePopulate = await checkForPopulate('stock price');
