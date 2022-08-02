1fec4d1d566e73d24908648c3b97e1c5d7f75fc7
James Spencer <jameskcs1254@gmail.com>
2022-08-02 15:04:55 +0100

    add conditional call for other assets

--- populateDb/populateDb.js
+++ populateDb/populateDb.js
@@ -1,9 +1,11 @@
-const populatePriceData = require('./populateStocks.js');
+const populatePriceData = require('./populateFromYahoo.js');
 const checkForPopulate = require('./cli.js');
 
 const main = async () => {
   const checkForStockPricePopulate = await checkForPopulate('stock price');
-  if (checkForStockPricePopulate == 'y') { populatePriceData(); }
+  if (checkForStockPricePopulate == 'y') { populatePriceData('stock'); }
+  const checkForOtherPricePopulate = await checkForPopulate('other asset price');
+  if (checkForOtherPricePopulate == 'y') { populatePriceData('other'); }
 }
 
 setTimeout(() => { //allow to connect to DBs before sending prompts
