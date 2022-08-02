bf1cacb113fb2c7f1b91c9dd1abd3ed6e536ae21
James Spencer <jameskcs1254@gmail.com>
2022-08-02 13:18:54 +0100

    create simple cli

---
+++ populateDb/cli.mjs
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+  
+const checkForPopulate = async (nameOfDataSet) => {
+  console.log(gradient.passion(`Populate ${nameOfDataSet}`));
+
+  let { response } = await inquirer.prompt({
+      type: 'input',
+      name: 'response',
+      message: `Would you like to populate the ${nameOfDataSet} dataset? (y/n)`
+  });
+
+  if (response.toLowerCase() == 'y' || response.toLowerCase() == 'n') {
+    console.log(gradient.passion('Response confirmed.'));
+    return response.toLowerCase();
+  } else {
+    console.log('Only input "y" or "n". Try again.')
+    checkForPopulate();
+  }
+}
+
+export default checkForPopulate;
