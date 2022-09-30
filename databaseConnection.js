c9fc9ca54b2b9be4dff8ccd637306c294bff77c8
James Spencer <jameskcs1254@gmail.com>
2022-09-30 19:41:58 +0100

    Create database connection

---
+++ databaseConnection.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: './mainDB.db'
+});
+
+
+const check = async () => {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+check();
+
+module.exports = db;
