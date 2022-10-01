1603ed6f77e3bf775bb8aeeb7c28acb393756043
James Spencer <jameskcs1254@gmail.com>
2022-10-01 10:15:47 +0100

    fix imports and main function call

--- populateDb/populateDb.js
+++ populateDb/populateDb.js
@@ -1,20 +1,21 @@
-const populateFromYahoo = require('./populateFromYahoo.js');
+const populateFromYahooData = require('./populateFromYahoo.js');
+const populateTickerListAll = require('./populateTickerList');
+const populateEarningsData = require('./populateEarnings')
+const populateEconomicData = require('./populateEconomicData')
 const checkForPopulate = require('./cli.js');
 
 const main = async () => {
+  const checkForTickerListPopulate = await checkForPopulate('ticker list')
   const checkForStockPricePopulate = await checkForPopulate('stock price');
   const checkForOtherPricePopulate = await checkForPopulate('other asset price');
-  //const checkForEarningsDataPopulate = await checkForPopulate('stock earnings');
-  //const checkForEconomicDataPopulate = await checkForPopulate('economic');
+  const checkForEarningsDataPopulate = await checkForPopulate('stock earnings');
+  const checkForEconomicDataPopulate = await checkForPopulate('economic');
 
+  if (checkForTickerListPopulate == 'y') { await populateTickerListAll('stocks'); }
   if (checkForStockPricePopulate == 'y') { await populateFromYahooData('stocks'); }
   if (checkForOtherPricePopulate == 'y') { await populateFromYahooData('other'); }
-  //if (checkForOtherPricePopulate == 'y') { populateEarnings(); }
-  //if (checkForOtherPricePopulate == 'y') { populateEconomicData(); }
+  if (checkForEarningsDataPopulate== 'y') { populateEarningsData(); }
+  if (checkForEconomicDataPopulate == 'y') { populateEconomicData(); }
 }
 
-setTimeout(() => { //allow to connect to DBs before sending prompts
-  main();
-}, 1)
-
-
+main();
