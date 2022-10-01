e7c406bf18eaf30fb4c1cab9cf34c6ea119d4af0
James Spencer <jameskcs1254@gmail.com>
2022-10-01 08:52:27 +0100

    Edit console.log

--- databaseConnection.js
+++ databaseConnection.js
@@ -9,7 +9,7 @@ const db = new Sequelize({
 const check = async () => {
   try {
     await db.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log('Connection to database has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
