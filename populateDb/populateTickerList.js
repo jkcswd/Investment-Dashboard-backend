16243cc38dd8547899028f3375a0bdd000012ce8
James Spencer <jameskcs1254@gmail.com>
2022-10-01 10:18:27 +0100

    Create db seeder for tickers from csv

---
+++ populateDb/populateTickerList.js
@@ -0,0 +1,33 @@
+const TickerList = require("../models/TickerList");
+const { csvToArray } = require("./utilities");
+
+const updateTickerList = async (csvPath, type, dataSource) => {
+  const tickerArray = csvToArray(csvPath);
+
+  try {
+    await TickerList.sync();
+  } catch (err) {
+    console.log(err);
+  }
+
+  for (ticker of tickerArray) {
+    try {
+      await TickerList.create({
+        ticker,
+        type ,
+        dataSource
+      },{ logging: false });
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+
+const updateTickerListAll = () => {
+  updateTickerList('./jsonAndCsv/wilshire5000Stocks.csv' , 'stock', 'yahoo');
+  updateTickerList('./jsonAndCsv/otherAssets.csv', 'other', 'yahoo');
+  updateTickerList('./jsonAndCsv/wilshire5000Stocks.csv', 'economic', 'fred');
+}
+
+module.exports = updateTickerListAll;
