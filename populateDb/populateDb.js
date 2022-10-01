42245d87d2b874d6e1f24d1b685c829f85d2ac0c
James Spencer <jameskcs1254@gmail.com>
2022-10-01 14:38:25 +0100

    Uncomment

--- populateDb/populateDb.js
+++ populateDb/populateDb.js
@@ -1,18 +1,18 @@
 const populatePriceData = require('./populatePriceData.js');
 const populateTickerListAll = require('./populateTickerList');
-//const populateEarningsData = require('./populateEarnings')
+const populateEarningsData = require('./populateEarnings')
 //const populateEconomicData = require('./populateEconomicData')
 const checkForPopulate = require('./cli.js');
 
 const main = async () => {
   const checkForTickerListPopulate = await checkForPopulate('ticker list')
   const checkForPricePopulate = await checkForPopulate('stock and other asset price data');
-  //const checkForEarningsDataPopulate = await checkForPopulate('stock earnings');
+  const checkForEarningsDataPopulate = await checkForPopulate('stock earnings');
   //const checkForEconomicDataPopulate = await checkForPopulate('economic');
 
   if (checkForTickerListPopulate == 'y') { await populateTickerListAll(); }
   if (checkForPricePopulate == 'y') { await populatePriceData(); }
-  //if (checkForEarningsDataPopulate== 'y') { await populateEarningsData(); }
+  if (checkForEarningsDataPopulate== 'y') { await populateEarningsData(); }
   //if (checkForEconomicDataPopulate == 'y') { await populateEconomicData(); }
 }
 
