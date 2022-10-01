9b5cfd46490bf5fbb103357ae1a1778bb78f0a3a
James Spencer <jameskcs1254@gmail.com>
2022-10-01 11:41:53 +0100

    fix import

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { db } = require('../databaseConnection');
+const  db  = require('../databaseConnection');
 
 const User = db.define('User', {
   username:{ type: DataTypes.STRING },
