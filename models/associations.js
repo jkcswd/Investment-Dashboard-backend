89b656f8fc6e1db18e9d4f79bc2b2f049773bf2d
James Spencer <jameskcs1254@gmail.com>
2022-10-01 11:26:23 +0100

    Add User to associations

--- models/associations.js
+++ models/associations.js
@@ -3,6 +3,9 @@ const EconomicData = require('./EconomicData');
 const EarningsData = require('./EarningsData');
 const TickerList = require('./TickerList')
 
+// The following models do not have relationships but are routed through this file for consistency of access.
+const User = require('./User');
+
 TickerList.hasMany(PriceData);
 TickerList.hasMany(EarningsData);
 TickerList.hasMany(EconomicData);
@@ -11,4 +14,4 @@ EconomicData.belongsTo(TickerList);
 PriceData.belongsTo(TickerList);
 
 
-module.exports = { PriceData, EarningsData, EconomicData, TickerList }
+module.exports = { PriceData, EarningsData, EconomicData, TickerList, User }
