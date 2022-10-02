a2e0dbc721a3c429493baf095164ccdd9b7841be
James Spencer <jameskcs1254@gmail.com>
2022-10-02 10:03:10 +0100

    Rewrite schema in mongoose

--- models/TickerList.js
+++ models/TickerList.js
@@ -1,11 +1,14 @@
-const { DataTypes } = require('sequelize');
-const  db  = require('../databaseConnection');
+const mongoose = require('mongoose');
 
-const TickerList = db.define('TickerList', {
-  ticker:{ type: DataTypes.STRING },
-  type:{ type: DataTypes.STRING },
-  dataSource:{ type: DataTypes.STRING }
+const Schema = mongoose.Schema;
+
+const TickerListSchema = new Schema({
+  ticker: { type: String, required: true },
+  type: { type: String, required: true },
+  dataSource: { type: String, required: true },
+  earnings: { type: Schema.Types.ObjectId, ref: 'Earnings', required: false },
+  price: { type: Schema.Types.ObjectId, ref: 'Price', required: false },
+  economic: { type: Schema.Types.ObjectId, ref: 'Economic', required: false }
 });
 
-
-module.exports = TickerList;
+module.exports = mongoose.model('User', UserSchema);
