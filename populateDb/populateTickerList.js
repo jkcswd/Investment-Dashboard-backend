03be3a6022880dc1767fda43ab898dfc1a9608f8
James Spencer <jameskcs1254@gmail.com>
2022-10-02 10:38:26 +0100

    fix mongoose code

--- populateDb/populateTickerList.js
+++ populateDb/populateTickerList.js
@@ -1,16 +1,17 @@
-const TickerList = require("../models/TickerList");
+const Ticker = require("../models/Ticker");
 const { csvToArray } = require("./utilities");
 
 const updateTickerList = async (csvPath, type, dataSource) => {
   const tickerArray = csvToArray(csvPath);
-
   for (ticker of tickerArray) {
     try {
-      await TickerList.create({
+      const doc = new Ticker({
         ticker,
         type ,
         dataSource
       });
+
+      await doc.save();
     } catch (err) {
       console.log(err)
     }
