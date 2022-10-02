fd59ced49135bfe180ed2f49c411bac4fcadd210
James Spencer <jameskcs1254@gmail.com>
2022-10-02 09:49:37 +0100

    call database connection in app

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const connectDb = require('./databaseConnection');
 
 const symbolRouter= require('./routes/symbol');
+
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+connectDb();
 
 app.use('/symbol', symbolRouter)
 
