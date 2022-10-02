86c7fe42ee9bd28f68ed78f5adb2df78579eade2
James Spencer <jameskcs1254@gmail.com>
2022-10-02 10:16:06 +0100

    Update for mongoose

--- populateDb/populateTickerList.js
+++ populateDb/populateTickerList.js
@@ -4,19 +4,13 @@ const { csvToArray } = require("./utilities");
 const updateTickerList = async (csvPath, type, dataSource) => {
   const tickerArray = csvToArray(csvPath);
 
-  try {
-    await TickerList.sync();
-  } catch (err) {
-    console.log(err);
-  }
-
   for (ticker of tickerArray) {
     try {
       await TickerList.create({
         ticker,
         type ,
         dataSource
-      },{ logging: false });
+      });
     } catch (err) {
       console.log(err)
     }
