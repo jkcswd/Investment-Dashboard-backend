ae7ef7cb80f6527545967889e4c99e0c35133558
James Spencer <jameskcs1254@gmail.com>
2022-10-02 10:03:38 +0100

    fix import/export

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
@@ -7,4 +7,4 @@ const UserSchema = new Schema({
   email: { type: String, required: true }
 });
 
-export default mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
