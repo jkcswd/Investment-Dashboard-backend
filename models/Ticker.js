76c500787dbc6b35e713b93a824e78e95eca253c
James Spencer <jameskcs1254@gmail.com>
2022-10-03 17:52:23 +0100

    update ticker to use array of data references

--- models/Ticker.js
+++ models/Ticker.js
@@ -6,9 +6,9 @@ const Ticker = new Schema({
   ticker: { type: String, required: true },
   type: { type: String, required: true },
   dataSource: { type: String, required: true },
-  earnings: { type: Schema.Types.ObjectId, ref: 'Earnings', required: false },
-  price: { type: Schema.Types.ObjectId, ref: 'Price', required: false },
-  economic: { type: Schema.Types.ObjectId, ref: 'Economic', required: false }
+  earnings: [{ type: Schema.Types.ObjectId, ref: 'Earnings', required: false }],
+  price: [{ type: Schema.Types.ObjectId, ref: 'Price', required: false }],
+  economic: [{ type: Schema.Types.ObjectId, ref: 'Economic', required: false }]
 });
 
 module.exports = mongoose.model('Ticker', Ticker);
