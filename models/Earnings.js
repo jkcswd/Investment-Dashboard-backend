8ae45afd8151115753074233e5fe0471dafec349
James Spencer <jameskcs1254@gmail.com>
2022-10-04 20:08:09 +0100

    Add ticker and tickerId fields

--- models/Earnings.js
+++ models/Earnings.js
@@ -2,6 +2,8 @@ const { DataTypes } = require('sequelize');
 const db  = require('../databaseConnection');
 
 const EarningsData = db.define('EarningsData', {
+  tickerId: { type: Schema.Types.ObjectId, ref: 'Ticker' },
+  ticker: { type: String, required: false },
   date:{ type: DataTypes.DATE },
   epsActual:{ type: DataTypes.DOUBLE },
   epsEstimate:{ type: DataTypes.DOUBLE },
--- models/Economic.js
+++ models/Economic.js
@@ -2,6 +2,8 @@ const { DataTypes } = require('sequelize');
 const  db  = require('../databaseConnection');
 
 const EconomicData = db.define('EconomicData', {
+  tickerId: { type: Schema.Types.ObjectId, ref: 'Ticker' },
+  ticker: { type: String, required: false },
   date:{ type: DataTypes.DATE },
   value:{ type: DataTypes.DOUBLE },
 });
