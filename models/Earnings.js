23b5804ffea81b0840577cdcf3c4a874bf254763
James Spencer <jameskcs1254@gmail.com>
2022-10-04 20:55:51 +0100

    Add compound index

--- models/Earnings.js
+++ models/Earnings.js
@@ -10,4 +10,8 @@ const EarningsPerShareSchema = new Schema({
   epsEstimate:{ type: Number, required: false }
 });
 
+EarningsPerShareSchema.index({ ticker: 1, date: 1}, { unique: true });
+// Compound index with unique constraint to stop multiple writes of same data 
+// and improve reads as will generally be filtered on these fields.
+
 module.exports = mongoose.model('EarningsPerShare', EarningsPerShareSchema);
