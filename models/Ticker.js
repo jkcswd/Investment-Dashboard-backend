fb619f83dedd0b184a2cbb6bf5f8718da050b5bc
James Spencer <jameskcs1254@gmail.com>
2022-10-04 20:44:24 +0100

    Add unique constraint

--- models/Ticker.js
+++ models/Ticker.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Ticker = new Schema({
-  ticker: { type: String, required: true },
+  ticker: { type: String, required: true, unique: true},
   type: { type: String, required: true },
   dataSource: { type: String, required: true }
 });
