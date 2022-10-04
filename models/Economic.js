d0a7cb773c6593153c70c538f21002064dc127a5
James Spencer <jameskcs1254@gmail.com>
2022-10-04 21:28:11 +0100

    Convert to mongoose

--- models/Economic.js
+++ models/Economic.js
@@ -1,11 +1,16 @@
-const { DataTypes } = require('sequelize');
-const  db  = require('../databaseConnection');
+const mongoose = require('mongoose');
 
-const EconomicData = db.define('EconomicData', {
+const Schema = mongoose.Schema;
+
+const EconomicSchema = new Schema({
   tickerId: { type: Schema.Types.ObjectId, ref: 'Ticker' },
   ticker: { type: String, required: false },
-  date:{ type: DataTypes.DATE },
-  value:{ type: DataTypes.DOUBLE },
+  date: { type: Date, required: false },
+  value: { type: Number, required: false },
 });
 
-module.exports = EconomicData;
+EconomicSchema.index({ ticker: 1, date: 1}, { unique: true });
+// Compound index with unique constraint to stop multiple writes of same data 
+// and improve reads as will generally be filtered on these fields.
+
+module.exports = mongoose.model('Price', PriceSchema);
