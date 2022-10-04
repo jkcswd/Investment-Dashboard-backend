f655d8abd87bb4bc4cf4032546c1c73f3f6bf9b6
James Spencer <jameskcs1254@gmail.com>
2022-10-04 12:48:39 +0100

    remove console.log

--- populateDb/populateDb.js
+++ populateDb/populateDb.js
@@ -1,22 +1,28 @@
 const populatePriceData = require('./populatePriceData.js');
 const populateTickerListAll = require('./populateTickerList');
-const populateEarningsData = require('./populateEarnings')
+//const populateEarningsData = require('./populateEarnings')
 //const populateEconomicData = require('./populateEconomicData')
 const checkForPopulate = require('./cli.js');
+const mongoose = require('mongoose')
 const connectDb = require('../databaseConnection.js');
 
-connectDb();
+
 
 const main = async () => {
   const checkForTickerListPopulate = await checkForPopulate('ticker list')
   const checkForPricePopulate = await checkForPopulate('stock and other asset price data');
-  const checkForEarningsDataPopulate = await checkForPopulate('stock earnings');
+  //const checkForEarningsDataPopulate = await checkForPopulate('stock earnings');
   //const checkForEconomicDataPopulate = await checkForPopulate('economic');
+
+
+  connectDb();
 
   if (checkForTickerListPopulate == 'y') { await populateTickerListAll(); }
   if (checkForPricePopulate == 'y') { await populatePriceData(); }
-  if (checkForEarningsDataPopulate== 'y') { await populateEarningsData(); }
+  //if (checkForEarningsDataPopulate== 'y') { await populateEarningsData(); }
   //if (checkForEconomicDataPopulate == 'y') { await populateEconomicData(); }
+
+  mongoose.connection.close()
 }
 
 main();
