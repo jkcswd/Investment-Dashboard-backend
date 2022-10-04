4bf5ae5fcb33ed75985084376957e7c1300f52bd
James Spencer <jameskcs1254@gmail.com>
2022-10-04 11:20:01 +0100

    add ticker and adjClose fields

--- models/Price.js
+++ models/Price.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PriceSchema = new Schema({
+  ticker: { type: Date, required: false },
   date: { type: Date, required: false },
   open: { type: Number, required: false },
   high: { type: Number, required: false },
   low: { type: Number, required: false },
   close: { type: Number, required: false },
+  adjClose: { type: Number, required: false },
   volume: { type: Number, required: false }
 });
 
