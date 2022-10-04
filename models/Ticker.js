5a38ece5aed8b4308b1d892596cf378b95315676
James Spencer <jameskcs1254@gmail.com>
2022-10-04 12:56:57 +0100

    remove array of refs

--- models/Ticker.js
+++ models/Ticker.js
@@ -5,10 +5,7 @@ const Schema = mongoose.Schema;
 const Ticker = new Schema({
   ticker: { type: String, required: true },
   type: { type: String, required: true },
-  dataSource: { type: String, required: true },
-  earnings: [{ type: Schema.Types.ObjectId, ref: 'Earnings', required: false }],
-  price: [{ type: Schema.Types.ObjectId, ref: 'Price', required: false }],
-  economic: [{ type: Schema.Types.ObjectId, ref: 'Economic', required: false }]
+  dataSource: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Ticker', Ticker);
