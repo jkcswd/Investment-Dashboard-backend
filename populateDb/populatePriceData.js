ade91e58af59b7f35dd0804a2f0afa022d9c1ded
James Spencer <jameskcs1254@gmail.com>
2022-10-04 11:19:47 +0100

    change to mongodb populate

--- populateDb/populatePriceData.js
+++ populateDb/populatePriceData.js
@@ -1,39 +1,42 @@
 const yahooFinance = require('yahoo-finance2').default;
+const Price = require('../models/Price.js');
+const Ticker = require('../models/Ticker.js');
 const {percentProgressDisplay, missingTickersToJson } = require('./utilities.js')
-const { PriceData, TickerList}  = require('../models/associations');
 
 const missingTickers = [];
 
 const getTickerPriceHistory = async (ticker) => {
   try {
-    const results = await yahooFinance.historical(ticker, { period1: '1900-01-01' });
-    const tickerPriceObj = { name: ticker, results };
-    
-    return tickerPriceObj;
+    const data = await yahooFinance.historical(ticker, { period1: '1900-01-01' });
+
+    return data;
   } catch (err) {
     console.log(err.message);
     missingTickers.push(ticker);
   }
 }
 
-const addPriceDataToDb = async (ticker, fKey) => { 
+const addPriceDataToDb = async (ticker) => { 
   try {
-    const tickerPriceObj = await getTickerPriceHistory(ticker);
-    if (tickerPriceObj) {
-      for (result of tickerPriceObj.results) {
-          await PriceData.findOrCreate({
-            where: { date: result.date, TickerListId: fKey },
-            defaults: {
-              date: result.date,
-              open: result.open,
-              high: result.high,
-              low: result.low,
-              close: result.close,
-              volume: result.volume,
-              TickerListId: fKey
-            },
-            logging: false 
+    const data = await getTickerPriceHistory(ticker);
+    if (data) {
+      for (day of data) {
+        try {
+          const doc = new Price({
+            ticker,
+            date: day.date,
+            open: day.open,
+            high: day.high,
+            low: day.low,
+            close: day.close,
+            adjClose: day.adjClose,
+            volume: day.volume
           });
+    
+          await doc.save();
+        } catch (err) {
+          console.log(err)
+        }
         }
     }
   } catch (err) {
@@ -43,16 +46,13 @@ const addPriceDataToDb = async (ticker, fKey) => {
 
 const populatePriceData = async () => {
   let counter = 0;
-  const tickerArray = await TickerList.findAll({
-    attributes: ['ticker', 'id'],
-    where: { dataSource: 'yahoo' }
-  });
-
-  await PriceData.sync();
+  const tickerArray = Ticker.find({ dataSource:'yahoo' });
+  console.log(tickerArray)
 
   for (const tickerData of tickerArray) { 
     try {
       await addPriceDataToDb(tickerData.dataValues.ticker, tickerData.dataValues.id);
+      //populate the ref array of ticker
       counter++;
       percentProgressDisplay(( counter / tickerArray.length ) * 100);
     } catch (err) {
@@ -64,3 +64,17 @@ const populatePriceData = async () => {
 }
 
 module.exports = populatePriceData;
+
+
+/*
+  {
+    date: 1981-05-06T00:00:00.000Z,
+    open: 0.122768,
+    high: 0.122768,
+    low: 0.12221,
+    close: 0.12221,
+    adjClose: 0.095255,
+    volume: 18950400
+  },
+*/
+
