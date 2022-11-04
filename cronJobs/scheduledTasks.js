8566e9f20d9d5f8f543e66a11ce62831a684813a
James Spencer <jameskcs1254@gmail.com>
2022-11-04 14:29:12 +0000

    fix string

--- cronJobs/scheduledTasks.js
+++ cronJobs/scheduledTasks.js
@@ -2,14 +2,14 @@ const cron = require('node-cron');
 
 const scheduledTasks = () => {
   // Every weekday tasks
-  cron.schedule('* * * * Monday, Tuesday, Wednesday, Thursday, Friday', () => {
+  cron.schedule('* * * * * Monday,Tuesday,Wednesday,Thursday,Friday', () => {
     updateDb();
   });
 
 
   // Monthly tasks
 
-  cron.schedule('* * 1 * *', () => {  
+  cron.schedule('* * * 1 * *', () => {  
     
   });
 }
