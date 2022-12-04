35f6e03a142dbe9d9ca1af3d8acbefa9c0df4de9
James Spencer <jameskcs1254@gmail.com>
2022-12-04 15:18:37 +0000

    fix url

--- routes/dailyReport.js
+++ routes/dailyReport.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const dailyReportController = require('../controllers/dailyReportController');
 
-router.get("/daily-report", dailyReportController);
+router.get("/", dailyReportController);
 
 module.exports = router;
