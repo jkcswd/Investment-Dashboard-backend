adf0b2a87eb85d71f25a8f90c9cfe3a04b4735a7
James Spencer <jameskcs1254@gmail.com>
2022-12-04 15:19:14 +0000

    add db call and return data

--- controllers/dailyReportController.js
+++ controllers/dailyReportController.js
@@ -1,5 +1,13 @@
-const dailyReportController = async (req,res,next) => {
+const DailyReport = require('../models/DailyReport')
 
+const dailyReportController = async (req,res,next) => {
+  try {
+    const data = await DailyReport.find().sort({ date: -1 }).limit(1)
+        
+    res.json(data);
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = dailyReportController;
