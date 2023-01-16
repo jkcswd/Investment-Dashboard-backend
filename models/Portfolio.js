b7b5d53e41f699fd5688ae14357ceba3fa13fd5f
James Spencer <jameskcs1254@gmail.com>
2023-01-16 19:22:32 +0000

    fix spelling

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const PortfolioSchema = new Schema({
   portfolioName: { type: String, required: false },
   dateOpened: { type: Date, required: false },
-  initalCapital: { type: Number, required: false },
+  initialCapital: { type: Number, required: false },
   currentCapital: { type: Number, required: false },
 });
 
